a05312a6106a8563c744eac7eed76962733af9fc
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-03-14 15:26:37 -0300

    Inicio do curso de programação funcional da Alura, com Clojure.

---
+++ src/forca/core.clj
@@ -0,0 +1,28 @@
+(ns forca.core
+  (:gen-class))
+
+(def total-de-vidas 6)
+
+(defn perdeu [] (print "Você perdeu"))
+
+;(defn jogo [vidas]
+  ;(if (= vidas 0)
+    ;(perdeu)
+    ;(jogo (- vidas 1))
+  ;)
+;)
+
+(defn jogo [vidas]
+  (if (= vidas 0)
+    (perdeu)
+    (do
+      (print vidas)
+      (jogo (dec vidas))
+    )
+  )
+)
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
