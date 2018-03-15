2514b62ec80d032cf4a8023d728a4bccbf216e39
Daniel Mendes de Melo Sousa <daniel.mendes@sp01.local>
2018-03-15 14:58:56 -0300

    Alteração para que a pilha de chamada fique menor

--- src/forca/core.clj
+++ src/forca/core.clj
@@ -27,20 +27,25 @@
   (.contains palavra chute)
 )
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)
-  )
-)
+;Função movidas para dentro da jogo para evitar o acumulo da pilha de chamadas
+;(defn avalia-chute [chute vidas palavra acertos]
+  ;(if (acertou? chute palavra)
+    ;(jogo vidas palavra (conj acertos chute))
+    ;(jogo (dec vidas) palavra acertos)
+  ;)
+;)
 
 (defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-a-palavra? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos)
-    )
+  (cond
+    (= vidas 0) (perdeu)
+    (acertou-a-palavra? palavra acertos) (ganhou)
+    :else
+      (let [chute (le-letra!)]
+        (if (acertou? chute palavra)
+          (jogo vidas palavra (conj acertos chute))
+          (jogo (dec vidas) palavra acertos)
+        )
+      )
   )
 )
 
